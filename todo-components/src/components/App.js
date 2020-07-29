95d9e2f7396784bc0041c89d302431a13d53a1ce
harpreet <Harpreet Kour>
2020-07-29 11:52:48 -0600

    Add the 'remove to-do' feature to the ToDo component.

--- todo-components/src/components/App.js
+++ todo-components/src/components/App.js
@@ -32,11 +32,7 @@ class App extends React.Component {
     // this.setState() triggers the render() method, so we can see updated state info in our presentation.
     this.setState( {[key]: value} );
   }
-  removeToDo ( id )
-  {
-     // Using dispatch with our remove action.
-     this.props.dispatch( removeToDo( id ) );
-  }
+  
   render ()
   {
     return (
@@ -58,7 +54,10 @@ class App extends React.Component {
           <h2>Current To-Dos:</h2>
           <ul>
             {this.props.toDos.map( toDo => ( // We can use .map() to "loop" through our array contents. Great for outputting something like these ToDos.
-                  <ToDo key={toDo.uniqueId} text={toDo.value} /> 
+                    <ToDo
+                    key={toDo.uniqueId} 
+                    uniqueId={toDo.uniqueId}
+                    text={toDo.value} /> 
               ) )}
           </ul>
       </>
